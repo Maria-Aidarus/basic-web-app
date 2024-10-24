a842abc1c44c8f8f823d06fbaf44c1d42ef75f7e
Maria Aidarus <mariaaidarus@dyn-172-20-100-44.qatar.cmu.local>
2024-10-24 16:16:54 +0300

    name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -10,6 +10,10 @@ export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("andrewid")) {
     return "andrewID: maidarus"; // Correctly format the output
   }
-
+  
+  if (query.includes("What is your name?")) {
+    return "maidarus";
+  }
+  
   return "";
 }
